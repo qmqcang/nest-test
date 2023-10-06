220275f7aceee8ae4c73fabc6467956b282dee64
qmqc <2335948360@qq.com>
2023-10-07 00:08:35 +0800

    重构数据库连接

---
+++ src/logs/logs.module.ts
@@ -0,0 +1,45 @@
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { WinstonModule, WinstonModuleOptions, utilities } from 'nest-winston'
+import * as winston from 'winston'
+import 'winston-daily-rotate-file'
+import { LogEnum } from '../enum/config.menu'
+
+@Module({
+  imports: [
+    WinstonModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          transports: [
+            new winston.transports.Console({
+              level: 'info',
+              format: winston.format.combine(
+                winston.format.timestamp(),
+                utilities.format.nestLike()
+              )
+            }),
+            ...(configService.get(LogEnum.LOG_ON)
+              ? [
+                  new winston.transports.DailyRotateFile({
+                    level: configService.get(LogEnum.LOG_LEVEL),
+                    dirname: 'logs',
+                    filename: 'application-%DATE%.log',
+                    datePattern: 'YYYY-MM-DD',
+                    zippedArchive: true,
+                    maxSize: '20m',
+                    maxFiles: '14d',
+                    format: winston.format.combine(
+                      winston.format.timestamp(),
+                      winston.format.simple()
+                    )
+                  })
+                ]
+              : [])
+          ]
+        } as WinstonModuleOptions)
+    }),
+    LogsModule
+  ]
+})
+export class LogsModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 import { AppModule } from './app.module'
@@ -6,7 +5,6 @@ import { HttpExceptionFilter } from './filters/http-exception.filter'
 import 'dotenv/config'
 
 async function bootstrap() {
-  // const logger = new Logger('Main')
   const app = await NestFactory.create(AppModule, {})
   const WinstonLogger = app.get(WINSTON_MODULE_NEST_PROVIDER)
 
